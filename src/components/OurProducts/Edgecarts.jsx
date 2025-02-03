f68c208ef8dbba5a64557369f0ab0fde5f46ddec
veerendra19codes <veerendragumate@gmail.com>
2025-02-03 13:52:05 +0530

    edgecarts contact us

--- src/components/OurProducts/Edgecarts.jsx
+++ src/components/OurProducts/Edgecarts.jsx
@@ -241,9 +241,9 @@ Download STRIKES now and be part of the future of collaboration!
                     </h2>
                 </div>
 
-                <Link to="/contactus" className="rounded-full p-2   pl-6 text-md md:text-xl whitespace-nowrap text-black bg-customgreen flex gap-4 justify-center items-center shadow-md shadow-customgreen transition-all duration-1000 ease-out delay-500  hover:shadow-none group my-4 font-semibold">
+                <Link to="/contactus" className="rounded-full p-2  pl-4 md:pl-6 text-md md:text-xl whitespace-nowrap text-black bg-customgreen flex gap-4 justify-center items-center shadow-md shadow-customgreen transition-all duration-1000 ease-out delay-500  hover:shadow-none group my-4 font-semibold">
                     Contact Us 
-                    <FaArrowRightLong className="bg-white text-customgray rounded-full size-10 p-3 font-thin transition-transform -rotate-45 group-hover:rotate-0" /> 
+                    <FaArrowRightLong className="bg-white text-customgray rounded-full size-6 md:size-10 p-1 md:p-3 font-thin transition-transform -rotate-45 group-hover:rotate-0" /> 
                 </Link>
                 
             </div>
